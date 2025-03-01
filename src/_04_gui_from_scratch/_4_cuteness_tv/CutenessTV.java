360f07a78309ac8c9c05d0325cb1940f3fd23532
league <league@imac-16.attlocal.net>
2025-03-01 13:38:24 -0800

    I finished the module!

--- src/_04_gui_from_scratch/_4_cuteness_tv/CutenessTV.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/CutenessTV.java
@@ -2,6 +2,7 @@ package _04_gui_from_scratch._4_cuteness_tv;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -33,8 +34,48 @@ public class CutenessTV implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getSource() == button1) {
+			showDucks();
+		}
+		else if(e.getSource() == button2) {
+			showFrog();
+		}
+		else if (e.getSource() == button3) {
+			showFluffyUnicorns();
+		}
 	}
 	
+	
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+	
+	
+	
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't 
+	     //work on some Linux implementations 
+	     try {
+	     if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	     if (Runtime.getRuntime().exec(new String[] { 
+	      "which", "xdg- open" }).getInputStream().read() != -1) {
+	     Runtime.getRuntime().exec(new String[] { "xdg-open", videoID }); 
+	     }
+	     } else {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     }      } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
 	
 }
